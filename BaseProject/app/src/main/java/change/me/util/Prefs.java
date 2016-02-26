cb21c4c68b9e87f388127c20db6cfdd137a7a277
Evgeniy Sharafan <evgeniysharafan@gmail.com>
2016-02-26 16:45:34 +0200

    Minor changes.

--- BaseProject/app/src/main/java/change/me/util/Prefs.java
+++ BaseProject/app/src/main/java/change/me/util/Prefs.java
@@ -1,29 +1,19 @@
 package change.me.util;
 
-import android.text.TextUtils;
-
 import com.evgeniysharafan.utils.PrefUtils;
 
 public final class Prefs {
 
     private static final String EXAMPLE = "example";
 
-    // cache for frequent usage
-    private static String example;
-
     private Prefs() {
     }
 
     public static String getExample() {
-        if (TextUtils.isEmpty(example)) {
-            example = PrefUtils.getString(EXAMPLE, "");
-        }
-
-        return example;
+        return PrefUtils.getString(EXAMPLE, "");
     }
 
     public static void setExample(String exam) {
-        example = exam;
         PrefUtils.put(EXAMPLE, exam);
     }
 
